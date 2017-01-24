d2c73e60298487905df1f228169b09d6aeb4da47
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-01-24 18:59:50 -0500

    semicolons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express')
+var express = require('express');
 var server = express();
 
-var port = 8080
+var port = 8080;
 
 server.get('/', function(request, response){
   response.sendFile('index.html', {root: __dirname + '/public/html'});
